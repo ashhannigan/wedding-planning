4ca32b6d2bb2f429d1c8a11eaf9891a802b99941
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-03-30 20:21:27 -0400

    login path updates and background image

---
+++ client/pages/login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import aiWedPic from '../images/AI-wed-background.png'; // Import image directly
+
+function Login() {
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        navigate("/home");
+    }
+
+    return (
+        <div className="login-page" style={{ backgroundImage: `url(${aiWedPic})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <span className="login-container">
+                <label>Username:</label>
+                <input type="email" id="email" name="email" />
+                <br />
+
+                <label className="password-login">Password:</label>
+                <input type="password" id="password" name="password" />
+                <button onClick={handleLogin}>Login</button>
+            </span>
+
+            <footer id="login-footer">
+                Don't have an account?<input type="checkbox" />
+                Forgot password?<input type="checkbox" />
+            </footer>
+        </div>
+    );
+}
+
+export default Login;
